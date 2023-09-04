6ed40dc1053ee1e1e95c123ebba5517403834a81
isaacmcallister843 <isaac.mcallister@gmail.com>
2023-09-04 02:12:21 +0100

    yay

--- Motors.py
+++ Motors.py
@@ -4,6 +4,7 @@ import adafruit_pca9685
 from adafruit_servokit import ServoKit
 import time 
 import numpy as np 
+import threading
 
 # Initialize the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -65,9 +66,9 @@ class arm:
 
         startingRealAngle = self.motorMatrix[2, motorNumber]
         angleChange = startingPos - targetPos
-        finalRealAngle = startingRealAngle + angleChange
+        finalRealAngle = startingRealAngle + angleChange * self.ccw
 
-        for i in range(startingRealAngle, finalRealAngle, it):
+        for i in range(startingRealAngle, finalRealAngle, it*self.ccw):
             time.sleep(.02) 
             self.motorKit.servo[self.motorMatrix[3, motorNumber]].angle = i 
 
@@ -76,23 +77,54 @@ class arm:
         self.motorMatrix[2, motorNumber] = finalRealAngle
         time.sleep(.5)
 
-
-
     
 topLeftArm = arm(3, 4, 5, kit)
 topLeftArm.calibrateArm(98, 0, 170,1)
 
 topRightArm = arm(12, 11, 10, kit)
-topRightArm.calibrateArm(12, 167, 30, 1)
+topRightArm.calibrateArm(12, 167, 30, -1)
 
 bottomLeftArm = arm(0, 1, 2, kit)
-bottomLeftArm.calibrateArm(65, 25, 150, 1)
+bottomLeftArm.calibrateArm(130, 25, 150, 1)
 
 bottomRightArm = arm(15, 14, 13, kit)
-bottomRightArm.calibrateArm(38, 170, 20, 1)
+bottomRightArm.calibrateArm(38, 170, 25, -1)
 
 
-topLeftArm.moveMotor(0, 90)
-print(topLeftArm.motorMatrix)
-topLeftArm.moveMotor(0, 0)
-print(topLeftArm.motorMatrix)
+def AllMotorTest():
+    bottomRightArm.moveMotor(0, 90)
+    bottomRightArm.moveMotor(1, 0)
+    bottomRightArm.moveMotor(2, 180)
+    bottomRightArm.moveMotor(2, 90)
+    bottomRightArm.moveMotor(1, 90)
+    bottomRightArm.moveMotor(0, 0)
+
+    topRightArm.moveMotor(0, 90)
+    topRightArm.moveMotor(1, 0)
+    topRightArm.moveMotor(2, 180)
+    topRightArm.moveMotor(2, 90)
+    topRightArm.moveMotor(1, 90)
+    topRightArm.moveMotor(0, 0)
+
+
+    topLeftArm.moveMotor(0, 90)
+    topLeftArm.moveMotor(1, 0)
+    topLeftArm.moveMotor(2, 180)
+    topLeftArm.moveMotor(2, 90)
+    topLeftArm.moveMotor(1, 90)
+    topLeftArm.moveMotor(0, 0)
+
+
+# Assuming topLeftArm is already defined
+
+# Create threads
+thread1 = threading.Thread(target=topLeftArm.moveMotor, args=(1, -40))
+thread2 = threading.Thread(target=topLeftArm.moveMotor, args=(2, 180))
+
+# Start threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to complete
+thread1.join()
+thread2.join()
